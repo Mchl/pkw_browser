38a5dfcb7ee81c656de1988a5f2939ae9d5ed55e
Michal Jarosz <michal.jarosz@stepstone.com>
2020-01-29 23:37:19 +0100

    Poprawki geokodowania

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const throttle = new SuperagentThrottle({
 
 const trimQuotes = string => string.replace(/^"/, "").replace(/"$/, "")
 
-const superAgentTransformStream = new require("stream").Transform({
+const TransformStream = require("stream").Transform
+
+const csvTransformStream = new TransformStream({
   transform: function transformer(chunk, encoding, callback) {
     const csv = chunk
       .toString()
@@ -62,9 +64,46 @@ const superAgentTransformStream = new require("stream").Transform({
       opisGranic,
     }
 
-    const requestPath = encodeURI(
-      `https://nominatim.openstreetmap.org/search/Poland/powiat ${obwod.powiat}/${obwod.miejscowosc}/${obwod.ulica}/${obwod.numerPosesji}?format=geojson`,
+    callback(false, JSON.stringify(obwod))
+  },
+})
+
+const powiatMiejski = powiat =>
+  powiat.slice(0, 1).toUpperCase() === powiat.slice(0, 1)
+
+const nominatimCall = (obwod, requestPath) =>
+  superagent
+    .get(requestPath)
+    .use(throttle.plugin())
+    .set(
+      "User-Agent",
+      "PKW Browser - contact developer at mail@michaljarosz.biz",
     )
+    .then(data => {
+      console.log({ geoJSON: data.text })
+      obwod.geoJSON = JSON.parse(data.text)
+      return obwod
+    })
+
+const superAgentTransformStream = new TransformStream({
+  objectMode: true,
+  transform: function transformer(chunk, encoding, callback) {
+    let obwod = JSON.parse(chunk)
+
+    if (obwod.geoJSON && obwod.geoJSON.features.length !== 0) {
+      callback(false, `${JSON.stringify(obwod)}\n`)
+      console.log("skipping...")
+      return
+    }
+
+    const powiatUri = encodeURIComponent(
+      powiatMiejski(obwod.powiat) ? obwod.powiat : `powiat ${obwod.powiat}`,
+    )
+    const miejscowoscUri = encodeURIComponent(obwod.miejscowosc)
+    const ulicaUri = encodeURIComponent(obwod.ulica)
+    const numerPosesjiUri = encodeURIComponent(obwod.numerPosesji)
+
+    const requestPath = `https://nominatim.openstreetmap.org/search/Poland/${powiatUri}/${miejscowoscUri}/${ulicaUri}/${numerPosesjiUri}?format=geojson`
 
     console.log({ requestPath })
 
@@ -78,13 +117,51 @@ const superAgentTransformStream = new require("stream").Transform({
       .then(data => {
         console.log({ geoJSON: data.text })
         obwod.geoJSON = JSON.parse(data.text)
-        callback(false, `${JSON.stringify(obwod)}\n`)
+        return obwod
       })
+      .then(obwod => {
+        if (
+          obwod.geoJSON.features.length === 0 &&
+          obwod.ulica.split(" ").length > 1
+        ) {
+          const ulicaSplit = obwod.ulica.split(" ")
+          const ulicaUri = encodeURIComponent(ulicaSplit[ulicaSplit.length - 1])
+          const requestPath = `https://nominatim.openstreetmap.org/search/Poland/${powiatUri}/${miejscowoscUri}/${ulicaUri}/${numerPosesjiUri}?format=geojson`
+
+          console.log({ requestPath })
+
+          return superagent
+            .get(requestPath)
+            .use(throttle.plugin())
+            .set(
+              "User-Agent",
+              "PKW Browser - contact developer at mail@michaljarosz.biz",
+            )
+            .then(data => {
+              console.log({ geoJSON: data.text })
+              obwod.geoJSON = JSON.parse(data.text)
+              return obwod
+            })
+        }
+        return obwod
+      })
+      .then(obwod => callback(false, `${JSON.stringify(obwod)}\n`))
       .catch(console.error)
   },
 })
 
-fs.createReadStream("./data/2019_pe/obwody_glosowania.csv")
-  .pipe(split())
-  .pipe(superAgentTransformStream)
-  .pipe(fs.createWriteStream("./data/2019_pe/obwody_glosowania.jsonl"))
+const inputPath = process.argv[2]
+const outputPath = process.argv[3]
+
+if (/csv$/.test(inputPath)) {
+  fs.createReadStream(inputPath)
+    .pipe(split())
+    .pipe(csvTransformStream)
+    .pipe(superAgentTransformStream)
+    .pipe(fs.createWriteStream(outputPath))
+} else if (/jsonl$/.test(inputPath)) {
+  fs.createReadStream(inputPath)
+    .pipe(split())
+    .pipe(superAgentTransformStream)
+    .pipe(fs.createWriteStream(outputPath))
+}
